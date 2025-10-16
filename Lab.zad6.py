dd99f086a4f881edc75952606e439765b3694311
ewelinacupak-collab <ewelinacupak@gmail.com>
2025-10-16 23:25:36 +0200

    Update Lab.zad6.py

--- Lab.zad6.py
+++ Lab.zad6.py
@@ -1,7 +1,5 @@
 # Zadanie 6
 
-print (end="\n")
-
 droga = float(input("Podaj drogę w km:"))
 spalanie = float(input("Podaj spalanie samochodu w litrach na 100 km:"))
 cena_paliwa_za_litr = 6.5
@@ -11,4 +9,5 @@ zuzycie_paliwa = droga * (spalanie / 100)
 koszt = zuzycie_paliwa * cena_paliwa_za_litr
 
 print ("Samochód spali", zuzycie_paliwa, "litrów paliwa", end=("\n"))
+
 print ("Koszt wyniesie", koszt, "zł")
