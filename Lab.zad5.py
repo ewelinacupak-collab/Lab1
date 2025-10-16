4c435fad08032bd4840e2f4a3492bfe138bb8b19
ewelinacupak-collab <ewelinacupak@gmail.com>
2025-10-16 23:25:13 +0200

    Update Lab.zad5.py

--- Lab.zad5.py
+++ Lab.zad5.py
@@ -1,7 +1,5 @@
 # Zadanie 5
 
-print (end="\n")
-
 bok_a = float(input("Podaj długość 1-szego boku prostokąta"))
 bok_b = float(input("Podaj długość 2-ego boku prostokąta"))
 
@@ -9,4 +7,5 @@ pole = (bok_a * bok_b)
 obwod = (2 * bok_a + 2 * bok_b)
 
 print ("Pole wynosi", pole, end=("\n"))
+
 print ("Obwód wynosi", obwod)
